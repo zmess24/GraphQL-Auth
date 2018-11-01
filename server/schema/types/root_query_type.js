17626c92b4657bb706c0093f995901e343c44c77
Zac Messinger <zac.messinger@gmail.com>
2018-10-31 19:42:06 -0700

    completed backend

--- server/schema/types/root_query_type.js
+++ server/schema/types/root_query_type.js
@@ -1,12 +1,18 @@
 const 
 	graphql = require('graphql'),
-	{ GraphQLObjectType, GraphQLID } = graphql;
+	{ GraphQLObjectType, GraphQLID } = graphql,
+	UserType = require('./user_type');
 
 const RootQueryType = new GraphQLObjectType({
     name: 'RootQueryType',
     fields: {
 	// GraphQl expects every type loaded into our application has at least one field assigned to it.
-      dummyField: { type: GraphQLID } 
+      user: {
+		type: UserType,
+		resolve(parentValue, args, req) {
+			return req.user;
+		}
+	  }
     }
 });
 
